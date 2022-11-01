f1dba66fc6d1a6182e3550056e2d66ca7900cda8
Artem Grokha <grokha.artem@gmail.com>
2022-11-01 17:21:50 +0200

    финальный код после тестов

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeImgGalaryRender = ({ preview, original, description }) => {
+  return `
+<div class="gallery__item">
+<a class="gallery__link" href"${original}">
+<img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`;
+};
+const imgGalary = galleryItems.map(makeImgGalaryRender).join(``);
+const divGalaryRef = document.querySelector(`.gallery`);
+divGalaryRef.innerHTML = imgGalary;
+
+divGalaryRef.addEventListener(`click`, onTagContainerClick);
+
+function onTagContainerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== `IMG`) return;
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+  console.log(divGalaryRef);
+}
